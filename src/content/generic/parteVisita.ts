ce7b05a1f8c2c5cd4ba19f7e0ee2f9dca7892156
Scripted <miquel@tramita.info>
2025-08-11 13:25:49 +0200

    Partevisita variables

--- src/content/generic/parteVisita.ts
+++ src/content/generic/parteVisita.ts
@@ -8,12 +8,14 @@ export const addParteVisitaButton = (ev: MouseEvent) => {
   const HTMLFirstElement = document.getElementById('tusplantillas1') as HTMLInputElement
   const ref = document.getElementById('js_card_visita_propiedad_ref')?.textContent
   const name = document.getElementById('no.cliente')?.textContent?.trim()
+  const phone = document.getElementById('no.demtelefono2')?.textContent?.trim()
   const agent = document.getElementById('js_card_visita_demanda_agente_info')?.children[0]?.children[0]?.textContent?.split(' ')[0]
+  const nDemanda = document.getElementById('js_card_visita_demanda_numdemanda')?.textContent
 
   // Crear el enlace
   const link = document.createElement('a')
   link.className = 'acciones__btn'
-  link.href = `${ADMIN_URL}/panel/herramientas/partes-de-visita?ref=${ref}&name=${name}&agent=${agent}`
+  link.href = `${ADMIN_URL}/panel/herramientas/partes-de-visita?ref=${ref}&name=${name}&agent=${agent}&phone=${phone}&nDemanda=${nDemanda}`
   link.target = '_blank'
   link.textContent = '⭐ Parte de visita nuevo'
   link.style.textDecoration = 'none'
